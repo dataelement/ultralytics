5479f9a9c560f4c15daae88d0e68ff6e24c28ee4
youjiachen <youmcgrady@126.com>
2024-11-16 13:03:26 +0800

    add layout pretrain

---
+++ scripts/layout/train_layout_pretrain.sh
@@ -0,0 +1,11 @@
+# finetune yolo11x seg on dataelem_layout
+python train.py \
+    --data docsynth300k \
+    --model_path /workspace/models/YOLO11/yolo11l.pt \
+    --epoch 30 \
+    --val 0 \
+    --image-size 1024\
+    --batch-size 32 \
+    --project layout_yolo11l_docsynth300k \
+    --plot 1 \
+    --device "4,5,6,7" 
