493788f91024d1032380797163634a89525eb93e
youjiachen <youmcgrady@126.com>
2024-11-16 13:01:33 +0800

    add close mosaic param for training

--- train.py
+++ train.py
@@ -10,18 +10,19 @@ if __name__ == "__main__":
     parser.add_argument('--model_path', default=None, required=True, type=str)
     parser.add_argument('--epoch', default=None, required=True, type=int)
     parser.add_argument('--optimizer', default='auto', required=False, type=str)
-    parser.add_argument('--momentum', default=0.9, required=False, type=float)
+    parser.add_argument('--momentum', default=0.937, required=False, type=float)
     parser.add_argument('--lr0', default=0.02, required=False, type=float)
     parser.add_argument('--warmup-epochs', default=3.0, required=False, type=float)
     parser.add_argument('--batch-size', default=16, required=False, type=int)
     parser.add_argument('--image-size', default=None, required=True, type=int)
     parser.add_argument('--mosaic', default=1.0, required=False, type=float)
+    parser.add_argument('--close_mosaic', default=10, required=False, type=int)
     parser.add_argument('--pretrain', default=None, required=False, type=str)
     parser.add_argument('--val', default=1, required=False, type=int)
     parser.add_argument('--plot', default=0, required=False, type=int)
     parser.add_argument('--project', default=None, required=True, type=str)
     parser.add_argument('--resume', action=argparse.BooleanOptionalAction)
-    parser.add_argument('--workers', default=16, required=False, type=int)
+    parser.add_argument('--workers', default=8, required=False, type=int)
     parser.add_argument('--device', default="0", required=False, type=str)
     parser.add_argument('--save-period', default=10, required=False, type=int)
     parser.add_argument('--patience', default=100, required=False, type=int)
@@ -64,4 +65,5 @@ if __name__ == "__main__":
         patience=args.patience,
         project=args.project,
         name=name,
+        close_mosaic=args.close_mosaic,
     )
